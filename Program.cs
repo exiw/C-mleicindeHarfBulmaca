9b2c1f9be48d916b17156c5ea0b2a85785b648e1
exiw <51192517+exiw@users.noreply.github.com>
2021-09-21 20:09:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CümleicindeHardBulmaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir kelime giriniz :");
+            string kelime = Console.ReadLine();
+            Console.Write("Lütfen aranacak karakteri giriniz :");
+            char aranan = Convert.ToChar(Console.ReadLine());
+            int sonuc = kelime.IndexOf(aranan);
+            if (sonuc == -1)
+            {
+                Console.WriteLine("Bulunamadı");
+            }
+            else
+            {
+                Console.WriteLine("Kelimenin {0}. sırasında bulndur", (++sonuc));
+            }
+            Console.ReadKey();
+        }
+    }
+}
